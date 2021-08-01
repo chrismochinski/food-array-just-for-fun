ff18019c67ffbd2e80ebc152be4b42900b046285
Chris Mo <cmochinski@gmail.com>
2021-07-31 21:22:57 -0500

    commit practice w/ week3 slack question - food array

---
+++ favoriteFoods.js
@@ -0,0 +1,14 @@
+console.log("Tonight, I'm going to make...");
+console.log(" ");
+
+let dinner = [ 'lean meat', 'farro rice', 'steamed broccoli'];
+
+for(healthy of dinner){
+  console.log('...'+ healthy);
+
+}
+console.log(' ');
+console.log( "Actually, I don't feel like", dinner[0] +'.');
+console.log( 'Come to think of it...');
+console.log( "...I don't feel like", dinner[1], 'or', dinner[2] ,'either.');
+console.log(' ');
